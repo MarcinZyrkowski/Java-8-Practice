c8044e15efb805ae7646c96da8c1788497731ddd
MarcinZyrkowski <marcinzyrkowski7@gmail.com>
2024-05-15 12:04:38 +0200

    dates time localization completion

--- src/main/java/org/example/datestimelocalization/DatesTimeLocalization.java
+++ src/main/java/org/example/datestimelocalization/DatesTimeLocalization.java
@@ -1,5 +1,7 @@
 package org.example.datestimelocalization;
 
+import java.text.NumberFormat;
+import java.text.ParseException;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -9,7 +11,7 @@ import java.util.ResourceBundle;
 
 public class DatesTimeLocalization {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         LocalDate date = LocalDate.now();
 
         // we can put any period here e.g. days, months, weeks, years
@@ -34,6 +36,34 @@ public class DatesTimeLocalization {
         rb = ResourceBundle.getBundle("org.example.datestimelocalization.javaBundle.Zoo", uk);
         System.out.println(rb.getString("hello"));
         System.out.println(rb.getString("open"));
+
+        int numerator = 100000;
+        int denominator = 3;
+        double result = (double) numerator /denominator;
+
+        // number formats
+        // us format
+        NumberFormat us = NumberFormat.getNumberInstance(Locale.US);
+        System.out.println("us: " + us.format(result));
+
+        // germany format
+        NumberFormat de = NumberFormat.getNumberInstance(Locale.GERMANY);
+        System.out.println("de: " + de.format(result));
+
+        // french canada format
+        NumberFormat ca = NumberFormat.getNumberInstance(Locale.CANADA_FRENCH);
+        System.out.println("ca: " + ca.format(result));
+
+        // currency format
+        double price = 48;
+        NumberFormat usPrice = NumberFormat.getCurrencyInstance(Locale.US);
+        System.out.println("us price: " + usPrice.format(price)); // $48.00
+
+        // parsing
+        String amount = "$92,807.99";
+        NumberFormat cf = NumberFormat.getCurrencyInstance();
+        double value = (Double) cf.parse(amount);
+        System.out.println(value); // 92807.99
     }
 
     public static LocalDate addPeriod(LocalDate date, Period period) {
