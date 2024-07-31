1ce79bf2c0e040c6662191d3dfa81d4e5a5a00a2
MarcinZyrkowski <marcinzyrkowski7@gmail.com>
2024-07-31 11:23:15 +0200

    concurrency

---
+++ src/main/java/org/example/concurrency/ConcurrencyExample1.java
@@ -0,0 +1,27 @@
+package org.example.concurrency;
+
+public class ConcurrencyExample1 {
+
+    public static void main(String[] args) {
+        (new ReadInventoryThread()).start();
+        new Thread(new PrintData()).start();
+        (new ReadInventoryThread()).start();
+    }
+
+    public static class PrintData implements Runnable {
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("printing record: " + i);
+            }
+        }
+    }
+
+    public static class ReadInventoryThread extends Thread {
+        public void run() {
+            System.out.println("Printing zoo inventory");
+        }
+    }
+
+}
