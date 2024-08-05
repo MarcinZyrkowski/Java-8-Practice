819a6be706168d64d32ab0dca94b896a2fbe0faa
MarcinZyrkowski <marcinzyrkowski7@gmail.com>
2024-08-05 15:19:50 +0200

    concurrency v6 & readme

---
+++ README.md
@@ -0,0 +1,5 @@
+## Purpose
+This project was created to practice java-8 features in process of preparing for passing OCP exam.
+
+## Inspiration
+Examples of code are similar or copied from `"OCP Oracle Certified Professional Java SE 8 II Study Guide"`
---
+++ src/main/java/org/example/concurrency/ConcurrencyExample6.java
@@ -0,0 +1,33 @@
+package org.example.concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ConcurrencyExample6 {
+
+    // race condition example
+    // prints different number sequence
+
+    private int sheepCount = 0;
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService service = null;
+        try {
+            // creates 20 threads
+            service = Executors.newFixedThreadPool(20);
+            ConcurrencyExample6 sheepManager = new ConcurrencyExample6();
+            for (int i = 0; i < 10; i++) {
+                service.submit(sheepManager::incrementAndReport);
+            }
+        } finally {
+            if (service != null) {
+                service.shutdown();
+            }
+        }
+    }
+
+    private void incrementAndReport() {
+        System.out.println((++sheepCount) + " ");
+    }
+
+}
