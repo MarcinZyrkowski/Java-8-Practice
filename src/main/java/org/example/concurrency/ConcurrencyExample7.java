da544c65bb34afa73330ba2884676cdf9d36ff12
MarcinZyrkowski <marcinzyrkowski7@gmail.com>
2024-08-05 15:34:10 +0200

    concurrency v7 and v8

---
+++ src/main/java/org/example/concurrency/ConcurrencyExample7.java
@@ -0,0 +1,34 @@
+package org.example.concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ConcurrencyExample7 {
+
+    // race condition example
+    // prints different number sequence (but each number appears)
+
+    private AtomicInteger sheepCount = new AtomicInteger(0);
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService service = null;
+        try {
+            // creates 20 threads
+            service = Executors.newFixedThreadPool(20);
+            ConcurrencyExample7 sheepManager = new ConcurrencyExample7();
+            for (int i = 0; i < 10; i++) {
+                service.submit(sheepManager::incrementAndReport);
+            }
+        } finally {
+            if (service != null) {
+                service.shutdown();
+            }
+        }
+    }
+
+    private void incrementAndReport() {
+        System.out.println(sheepCount.incrementAndGet() + " ");
+    }
+
+}
---
+++ src/main/java/org/example/concurrency/ConcurrencyExample8.java
@@ -0,0 +1,36 @@
+package org.example.concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ConcurrencyExample8 {
+
+    // race condition example
+    // prints different number sequence
+
+    private int sheepCount = 0;
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService service = null;
+        try {
+            // creates 20 threads
+            service = Executors.newFixedThreadPool(20);
+            ConcurrencyExample8 sheepManager = new ConcurrencyExample8();
+            for (int i = 0; i < 10; i++) {
+                service.submit(sheepManager::incrementAndReport);
+            }
+        } finally {
+            if (service != null) {
+                service.shutdown();
+            }
+        }
+    }
+
+    private void incrementAndReport() {
+        // lock and monitor sheepCount object
+        synchronized (this) {
+            System.out.println((++sheepCount) + " ");
+        }
+    }
+
+}
