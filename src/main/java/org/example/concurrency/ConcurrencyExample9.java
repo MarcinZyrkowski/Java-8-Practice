0481443b895fab222215f06571262ac3e636e28d
MarcinZyrkowski <marcinzyrkowski7@gmail.com>
2024-08-08 20:22:33 +0200

    concurrency v9 and v10

--- src/main/java/org/example/concurrency/ConcurrencyExample9.java
+++ src/main/java/org/example/concurrency/ConcurrencyExample9.java
@@ -10,9 +10,9 @@ public class ConcurrencyExample9 {
 
     // will not be printed in order
     private static void printParallelStream() {
-        Arrays.asList(1,2,3,4,5,6,7,8,9)
+        Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9)
                 .parallelStream()
-                .forEach(number -> System.out.print(number+" "));
+                .forEach(number -> System.out.print(number + " "));
     }
 
 }
