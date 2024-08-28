eced8d4c855b6abb43e6450f33d927ab5d533398
MarcinZyrkowski <marcinzyrkowski7@gmail.com>
2024-08-28 11:24:29 +0200

    added buffered in out streams

---
+++ src/main/java/org/example/io/IOExample3.java
@@ -0,0 +1,37 @@
+package org.example.io;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import lombok.SneakyThrows;
+
+public class IOExample3 {
+
+  @SneakyThrows
+  public static void main(String[] args) {
+    File source = new File("src/main/resources/io/example3/from.txt");
+    File destination = new File("src/main/resources/io/example3/to.txt");
+
+    copy(source, destination);
+  }
+
+  private static void copy(File source, File destination) throws IOException {
+    try (
+        InputStream in = new BufferedInputStream(new FileInputStream(source));
+        OutputStream out = new BufferedOutputStream(new FileOutputStream(destination))
+    ) {
+      byte[] buffer = new byte[1024];
+      int lenghtRead;
+      while ((lenghtRead = in.read(buffer)) > 0) {
+        out.write(buffer, 0, lenghtRead);
+        out.flush();
+      }
+    }
+  }
+
+}
