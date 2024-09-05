5a81bdec3cd0d716e3c333d7fd9110e392add355
MarcinZyrkowski <marcinzyrkowski7@gmail.com>
2024-09-05 15:34:34 +0200

    nio 2, example 2, accessingPathComponents

--- src/main/java/org/example/nio/NIOExample2.java
+++ src/main/java/org/example/nio/NIOExample2.java
@@ -6,12 +6,14 @@ import java.nio.file.Paths;
 public class NIOExample2 {
 
   public static void main(String[] args) {
-    viewingThePath();
+    String rootPath = "src/main/resources/nio/example2/cities.txt";
+    Path path = Paths.get(rootPath);
+
+    viewingThePath(path);
+    accessingPathComponents(path);
   }
 
-  private static void viewingThePath() {
-    Path path = Paths.get("src/main/resources/nio/example2/cities.txt");
-
+  private static void viewingThePath(Path path) {
     // string representation of the entire path
     System.out.println(path.toString());
 
@@ -19,6 +21,17 @@ public class NIOExample2 {
     for (int i = 0; i < path.getNameCount(); i++) {
       System.out.println("element i=" + i + " is: " + path.getName(i));
     }
+  }
+
+  private static void accessingPathComponents(Path path) {
+    Path fileName = path.getFileName();
+    System.out.println(fileName);
+
+    Path parent = path.getParent();
+    System.out.println(parent);
+
+    Path root = path.getRoot();
+    System.out.println(root); // returns null because path is relative
   }
 
 }
