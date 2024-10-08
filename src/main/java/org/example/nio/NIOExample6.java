a31d444d20e712b39d2e66074c48417c1dbcc9dc
MarcinZyrkowski <marcinzyrkowski7@gmail.com>
2024-10-08 14:21:49 +0200

    nio example 6

--- src/main/java/org/example/nio/NIOExample6.java
+++ src/main/java/org/example/nio/NIOExample6.java
@@ -4,19 +4,22 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributeView;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
 import lombok.SneakyThrows;
 
 public class NIOExample6 {
 
   @SneakyThrows
   public static void main(String[] args) {
-    readAttributes();
-  }
-
-  public static void readAttributes() throws IOException {
     Path path = Paths.get("src/main/resources/nio/example6/colors.txt");
 
+    readAttributes(path);
+    modifyAttributes(path);
+  }
+
+  public static void readAttributes(Path path) throws IOException {
     BasicFileAttributes data = Files.readAttributes(path, BasicFileAttributes.class);
 
     System.out.println("Is path a directory? :" + data.isDirectory());
@@ -28,6 +31,16 @@ public class NIOExample6 {
     System.out.println("Last modified date/time :" + data.lastModifiedTime());
     System.out.println("Last accessed date/time :" + data.lastAccessTime());
     System.out.println("Unique file identifier (if available) " + data.fileKey());
+  }
+
+  public static void modifyAttributes(Path path) throws IOException {
+    BasicFileAttributeView view = Files.getFileAttributeView(path, BasicFileAttributeView.class);
+
+    BasicFileAttributes data = view.readAttributes();
+
+    FileTime lastModifiedTime = FileTime.fromMillis(data.lastModifiedTime().toMillis() + 10_000);
+
+    view.setTimes(lastModifiedTime, null, null);
   }
 
 }
